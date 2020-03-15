f1ff75232559ef4fd5dab3e35e9e306066aa943f
kikisix <61208458+kikisix@users.noreply.github.com>
2020-03-15 19:22:28 +0300

    Frontend (#2)
    * Simple view for temp graph
    
    * Modularize package json
    
    * Create nodejs.yml
    
    Co-authored-by: iamthen0ise <thefiltdj@gmail.com>

---
+++ frontend/src/components/Temperature.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <canvas id="tempChart"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Temperature"
+};
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,81 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Chart from "chart.js";
+
+const axios = require("axios");
+
+new Vue({
+  el: "#app",
+  render: h => h(App),
+  mounted() {
+    axios
+      .get("https://still-mountain-17725.herokuapp.com/api/temp?limit=100&page=-1")
+      .then(response => {
+        this.dates = response.data.map(list => {
+          return list.date;
+        });
+
+        this.temps = response.data.map(list => {
+          return list.value;
+        });
+
+        var ctx = document.getElementById("tempChart");
+        this.chart = new Chart(ctx, {
+          type: "line",
+          data: {
+            labels: this.dates,
+            datasets: [
+              {
+                label: "Avg. Temp",
+                backgroundColor: "rgba(54, 162, 235, 0.5)",
+                borderColor: "rgb(54, 162, 235)",
+                fill: false,
+                data: this.temps
+              }
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: "Temperature @ Home"
+            },
+            scales: {
+              xAxes: [
+                {
+                  type: "time",
+                  time: {
+                    unit: "minute",
+                    displayFormats: {
+                      hour: "LL @ kk:mm"
+                    },
+                    tooltipFormat: "LL @ kk:mm"
+                  },
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Date/Time"
+                  }
+                }
+              ],
+              yAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Temperature (°С)"
+                  },
+                  ticks: {
+                    callback: function(value) {
+                      return value + "°С";
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        });
+      })
+      .catch(error => {
+        throw(error)
+      })
+      .finally(() => (this.loading = false));
+  }
+}).$mount("#app")
